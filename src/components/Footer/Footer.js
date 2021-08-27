5dc963223b3c458548cfcd5f4a39632679916a29
milan Katira <84834795+milankatira@users.noreply.github.com>
2021-08-27 13:11:13 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <hr style={{ width: "90%", marginTop: 20 }} />
       <span className="name">
         Made by{" "}
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+        <a href="https://github.com/milankatira/word-search.git" target="__blank">
           Milan katira
         </a>
       </span>
